363168ee6192df2def70cbdd12afdb303b5464f5
onixikum <dragonix1988@gmail.com>
2020-09-29 19:18:32 +0300

    Add index to users login

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-  field :user, [UserType], null: false,
+    field :user, [UserType], null: false
 
-  def user
-    User.find_by login: 'GIT_login'
+    def user
+      User.find_by(login: 'GIT_login_2')
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_132354) do
+ActiveRecord::Schema.define(version: 2020_09_29_155417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_132354) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["login"], name: "index_users_on_login", unique: true
   end
 
 end
